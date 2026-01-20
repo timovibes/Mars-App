4043674396f30948eb32a3c96f40415668694cff
timovibes <timothyator744@gmail.com>
2026-01-20 15:54:49 +0300

    Refactor NetworkMarsPhotosRepository to receive MarsApiService via constructor
    This covers:
    
    Passing MarsApiService into NetworkMarsPhotosRepository
    
    Updating getMarsPhotos() to use injected service
    
    Removing MarsApi object and unused imports

--- app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -20,9 +20,3 @@ interface MarsApiService{
     @GET("photos")
     suspend fun getPhotos():List<MarsPhoto>
 }
-
-object MarsApi{
-    val retrofitService : MarsApiService by lazy {
-        retrofit.create(MarsApiService::class.java)
-    }
-}
