e563cfc099eb131d9b03f6a458c6cb0828eb722d
timovibes <timothyator744@gmail.com>
2026-01-21 21:27:24 +0300

    refactor: Add repository to MarsViewModel constructor and implement Factory

--- app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -15,14 +15,21 @@
  */
 package com.example.marsphotos.ui.screens
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewModelScope
-import com.example.marsphotos.data.NetworkMarsPhotosRepository
-import com.example.marsphotos.network.MarsApi
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.marsphotos.MarsPhotosApplication
+import com.example.marsphotos.data.MarsPhotosRepository
 import kotlinx.coroutines.launch
+import okhttp3.WebSocket
 import java.io.IOException
 
 sealed interface MarsUiState {
@@ -31,7 +38,7 @@ sealed interface MarsUiState {
     object Loading : MarsUiState
 }
 
-class MarsViewModel : ViewModel() {
+class MarsViewModel(private val marsPhotosRepository : MarsPhotosRepository) : ViewModel() {
     /** The mutable State that stores the status of the most recent request */
     var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
         private set
@@ -50,7 +57,6 @@ class MarsViewModel : ViewModel() {
     fun getMarsPhotos() {
        viewModelScope.launch {
            marsUiState = try {
-               val marsPhotosRepository = NetworkMarsPhotosRepository()
                val listResult = marsPhotosRepository.getMarsPhotos()
                MarsUiState.Success("Success: ${listResult.size} Mars photos retrieved")
            } catch (e: IOException) {
@@ -58,5 +64,21 @@ class MarsViewModel : ViewModel() {
            }
        }
     }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                // 1. Grab the Application object using the APPLICATION_KEY
+                val application = (this[APPLICATION_KEY] as MarsPhotosApplication)
+
+                // 2. Get the repository from the application's container
+                val marsPhotosRepository = application.container.marsPhotosRepository
+
+                // 3. Construct and return the ViewModel
+                MarsViewModel(marsPhotosRepository = marsPhotosRepository)
+            }
+        }
+    }
+
 }
 
